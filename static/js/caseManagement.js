0d00d95cc929d219fee14c6b314f13a209766d24
chaiqingshan <1324315849@qq.com>
2017-12-28 21:04:19 +0800

    修改案例管理导出和筛选

--- static/js/caseManagement.js
+++ static/js/caseManagement.js
@@ -13,8 +13,9 @@ var app = new Vue({
         useStatus: [], // 案例状态
         missionList: [], //测试任务
         testpoint: '', // 测试点
-        author: '', //编写者
-        executor: '', //执行者
+        currentUserId: sessionStorage.getItem('userId'),//当前登录用户id
+        author: false, //编写者
+        executor: false, //执行者
         testdesign: '', //测试意图
         autid: '', //被测系统
         transid: '', //功能码
@@ -455,8 +456,8 @@ var app = new Vue({
                     'casecode': app.casecode,
                     'informationtype': 'testcase',
                     'testpoint': app.testpoint,
-                    'author': app.author,
-                    'executor': app.executor,
+                    'author': app.author?app.currentUserId:'',
+                    'executor': app.executor?app.currentUserId:'',
                     'testdesign': app.testdesign,
                     'autid': app.autid,
                     'transid': app.transid,
