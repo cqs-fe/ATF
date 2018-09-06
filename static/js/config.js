c0120fbba6973a667cb006bb0edb95fafe389750
glucky0898 <1377490634@qq.com>
2018-09-06 11:12:01 +0800

    接口和token

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 var address = "http://10.108.223.23:8080/ATFCloud/";
-var address2 = ' http://10.108.223.23:8080/atfcloud1.0a/';
+var address2 = ' http://10.108.223.23:8080/atfcloud2.0a/';
 var address3 = ' http://10.108.223.23:8080/atfcloud2.0a/';
 // var address = "/";
 // var address = "/";
